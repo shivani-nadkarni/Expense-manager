d0b5a9b48a5607038d6da795e9decad7111417a1
Shivani Nadkarni <shiv.nadk@gmail.com>
2020-11-18 16:11:33 +0530

    Removed unused constants and dependencies.

--- ExpenseManagerBackend/appConst.js
+++ ExpenseManagerBackend/appConst.js
@@ -2,15 +2,11 @@
 
 const mongoDbUrl = "mongodb://127.0.0.1:27017";
 const mongoDbName = "ExpenseDb";
-const dummyEmpApi = "http://dummy.restapiexample.com/api/v1/employee/1"
-const userList = "http://localhost:5000/userList"
 const Port = 8099;
 
 // export the constants
 module.exports = {
     mongoDbUrl,
     mongoDbName,
-    Port,
-    dummyEmpApi,
-    userList
+    Port
 };
